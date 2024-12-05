7310fe21932d6831eefbb1c6277068968f627425
Rahul Kumar <rahulkumar202134@gmail.com>
2024-12-05 19:31:47 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Navbar />
+
+          <div className="container">
+            <div className="row">
+              <div className="col-md">
+                <Routes>
+                  <Route path="/" element={<News key="general" category="general" />}/>
+                  <Route path="/Entertainment" element={<News key="entertainment" category="entertainment" />}/>
+                  <Route path="/Technology" element={<News key="technology" category="technology" />}/>
+                  <Route path="/Sports" element={<News key="sports" category="sports" />}/>
+                  <Route path="/Business" element={<News key="business" category="business" />}/>
+                  <Route path="/Health" element={<News key="health" category="health" />}/>
+                  <Route path="/Science" element={<News key="science" category="science" />}/>
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </BrowserRouter>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+	return (
+		<div>
+			<nav className="navbar navbar-expand-lg bg-dark">
+				<div className="container-fluid ">
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarNav"
+						aria-controls="navbarNav"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse"
+						id="navbarNav">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<Link className="nav-link active text-white"
+<<<<<<< HEAD
+									aria-current="page" to={`/`}>
+=======
+									
+									to="/">
+>>>>>>> c781b4b (final)
+									Home
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Entertainment`}>
+									Entertainment
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Technology`}>
+									Technology
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Sports`}>
+									Sports
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Business`}>
+									Business
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Health`}>
+									Health
+								</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="nav-link text-white" to={`/Science`}>
+									Science
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
+}
+
+export default Navbar;
